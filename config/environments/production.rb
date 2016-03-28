f0098c0f829184359ca1dbdfe8786b7751948fc4
laferrera <jason.laferrera@gmail.com>
2016-03-28 10:50:00 -0400

    cloudfront

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Rails.application.configure do
   Paperclip::Attachment.default_options.merge!(
     :storage => :s3,
     :bucket => ENV['S3_BUCKET'],
-    :url => "/system/:class/:attachment/:id/:style/:filename",
+    :path => "/system/:class/:attachment/:id/:style/:filename",
+    # :url => "/system/:class/:attachment/:id/:style/:filename",
     :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
